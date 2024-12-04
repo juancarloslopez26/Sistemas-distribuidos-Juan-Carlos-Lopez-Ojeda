8f3d50553dd9fa4df2702105eec2de8a4c8f6eee
juancarloslopez26 <157658272+juancarloslopez26@users.noreply.github.com>
2024-12-03 22:38:44 -0600

    Prueba3 (#17)
    * ¨Tarea2¨
    
    * Se realizo un hola mundo en un dockerfile
    
    * Se realizo un hola mundo en dockerfile
    
    * Se hizo un chat atraves de dos contenedores con el p2p
    
    * Se realizo un file download
    
    * Ae realizaron los cambios necesarios pero la verdad no pude hacer que jalara
    
    * Se intento hacer el metodo update
    
    * Se hizo todo de nuevo para la implementacion del metodo getbyid de una nueva base de datos de libros
    
    * Crear endpoint para obtener grupos por nombre
    
    * Se realizaron las modificaciones para implemetar el getbyname y la paginacion
    
    * Prueba
    
    * Se realizo el ultimo metodo para buscar por nombre exacto
    
    * Se realizo el ultimo metodo para buscar por nombre exacto

---
+++ ChatP2P/Peer.cs
@@ -0,0 +1,97 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatP2P;
+
+public class Peer
+{
+    private readonly TcpListener _listener;
+    private TcpClient? _client;
+    private const int Port = 8080;
+
+    public Peer() => _listener = new TcpListener(IPAddress.Any, Port);
+
+    public async Task ConnectToPeer(string ipAddress, string port)
+    {
+        try
+        {
+            _client = new TcpClient(ipAddress, Convert.ToInt32(port));
+            Console.WriteLine("Connected to peer :D");
+
+            var receiveTask = ReceiveMessage();
+            await SendMessage();
+            await receiveTask;
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Connection closed :c " + ex.Message);
+        }
+    }
+
+    public async Task StartListening()
+    {
+        try
+        {
+            _listener.Start();
+            Console.WriteLine("Listening for incoming connections...");
+
+            while (true)
+            {
+                _client = await _listener.AcceptTcpClientAsync();
+                Console.WriteLine("Connected to Peer :D");
+
+                var receiveTask = ReceiveMessage();
+                await SendMessage();
+                await receiveTask;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Connection closed :c " + ex.Message);
+        }
+    }
+
+   private async Task ReceiveMessage(){
+    try{
+        var stream = _client!.GetStream();
+        var reader = new StreamReader(stream, Encoding.UTF8);
+
+        string? message;
+        while ((message = await reader.ReadLineAsync()) != null){
+            Console.WriteLine($"Peer message: {message}");
+        }
+    }
+    catch (Exception ex){
+        Console.WriteLine($"Error receiving message: {ex.Message}");
+    }
+    finally{
+        Close();
+    }
+}
+
+   private async Task SendMessage(){
+    try{
+        var stream = _client!.GetStream();
+        var writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
+
+        string? message;
+        while ((message = Console.ReadLine()) != null){
+            await writer.WriteLineAsync(message);
+        }
+    }
+    catch (Exception ex){
+        Console.WriteLine($"Error sending messages: {ex.Message}");
+    }
+    finally{
+        Close();
+    }
+}
+
+    private void Close()
+    {
+        _client?.Close();
+        _listener.Stop();
+    }
+}
---
+++ RestApi/Services/IGroupService.cs
@@ -0,0 +1,11 @@
+using RestApi.Models;
+
+namespace RestApi.Services;
+
+public interface IGroupService{
+    Task<GroupUserModel> GetGroupByIdAsync (string Id, CancellationToken cancellationToken);
+    //paginacion tarea*
+    Task<IEnumerable<GroupUserModel>> GetGroupsByNameAsync(string name, int pageIndex, int pageSize, string orderBy, CancellationToken cancellationToken); 
+    Task DeleteGroupByIdAsync(string id, CancellationToken cancellationToken);
+    Task <GroupUserModel> CreateGroupAsync(string name, Guid[] users, CancellationToken cancellationToken);
+}
